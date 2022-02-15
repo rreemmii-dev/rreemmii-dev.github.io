c7a83ad30b39b7959a2ee63f0df3da42628e0e7c
RREEMMII <rreemmii-dev@users.noreply.github.com>
2022-02-15 16:23:24 +0100

    New way for smartphones

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
+// USELESS FILE - Not in the index.html header
+
+
+pixel_per_cm_detector_html = '<div style="visibility: collapse; width: 10cm; height: 10cm" id="pixel_per_cm_detector"></div>'
+
 function f() {
     var accordion_contents = document.getElementsByClassName('uk-accordion-content');
     var cards = document.getElementsByClassName("card");
-    if (window.innerWidth <= 1440) {
+    if (window.innerWidth / ppcm_x <= 15) {
         for (i = 0; i < accordion_contents.length; i++) {
             accordion_contents[i].style.flexDirection = 'column';
         }
@@ -16,7 +21,15 @@ function f() {
             cards[i].style.width = '25%';
         }
     }
-    console.log(window.innerWidth)
+    console.log(window.innerWidth / ppcm_x, ppcm_x);
 }
-window.addEventListener('resize', f);
-window.addEventListener('load', f)
+function get_ppcm() {
+    ppcm_detector = document.getElementById("pixel_per_cm_detector");
+    ppcm_x = ppcm_detector.getBoundingClientRect().width/10;
+    ppcm_detector.style.display = 'none';
+}
+window.addEventListener('load', function () {
+    get_ppcm();
+    f();
+    window.addEventListener('resize', f);
+})
