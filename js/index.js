add442c27d66dbccb9b7e64c29afad7e490d18cc
RREEMMII <rreemmii-dev@users.noreply.github.com>
2022-02-15 14:49:42 +0100

    Better for smartphones

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function f() {
+    var accordion_contents = document.getElementsByClassName('uk-accordion-content');
+    var cards = document.getElementsByClassName("card");
+    if (window.innerWidth <= 1440) {
+        for (i = 0; i < accordion_contents.length; i++) {
+            accordion_contents[i].style.flexDirection = 'column';
+        }
+        for (i = 0; i < cards.length; i++) {
+            cards[i].style.width = '90%';
+        }
+    } else {
+        for (i = 0; i < accordion_contents.length; i++) {
+            accordion_contents[i].style.flexDirection = 'row';
+        }
+        for (i = 0; i < cards.length; i++) {
+            cards[i].style.width = '25%';
+        }
+    }
+}
+
+window.setInterval(f, 1000);
